f6a5d56eff3d4367b5bc33302c4d8689f47193be
ufcovo <merttbulutt06@gmail.com>
2024-07-29 23:13:47 +0300

    Get started page

---
+++ lib/presentation/intro/pages/get_started.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 40),
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fill, image: AssetImage(AppImages.introBG))),
+            child: Column(
+              children: [
+                Align(
+                    alignment: Alignment.topCenter,
+                    child: SvgPicture.asset(AppVectors.logo)),
+                const Spacer(),
+                const Text(
+                  'Enjoy Listening To Music',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 18),
+                ),
+                const SizedBox(
+                  height: 21,
+                ),
+                const Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.grey,
+                      fontSize: 13),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                BasicAppButton(onPressed: () {}, title: 'Get Started')
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          )
+        ],
+      ),
+    );
+  }
+}
